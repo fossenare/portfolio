28e6f575f0ff5311425bfc087045fb7584740e85
Danil K~ <fossenare@gmail.com>
2023-07-17 12:35:49 +0500

    Co-authored-by: Galacticblack <Galacticblack@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // alert('hello')
 
 console.log('Hello Danil')
-
 console.log("Hello Viktor")
+
+
+const button = document.querySelector('button')
+button.innerHTML = 'Hello Danil'
+
+console.log(button)
