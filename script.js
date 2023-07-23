017af6217072b17eb899297dc0e3eb9abcf3d468
Danil K~ <fossenare@gmail.com>
2023-07-23 21:04:31 +0500

    new tranform and button.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // alert('hello')
 
-console.log('Hello Danil')
-console.log("Hello Viktor")
+console.log('Hello Danil!')
+console.log("Hello Viktor!")
+console.log('Hello My Project!')
 
 
 const button = document.querySelector('button')
 button.innerHTML = 'Hello Danil'
 
 console.log(button)
+
+const button= document.querySelector('python')
+button.innerHTML = 'еще я немного знаю Python'
+
+console.log(python)
